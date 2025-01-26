f309d2ad3a4f5d4f3b8d3192aef153f595fe8b1b
Gabriel Henrique <gabrielhenriquegh3878@gmail.com>
2025-01-25 22:07:24 -0300

    feat: add getById, deleteById, deleteAll, and updateById functions in 'contact' controller

--- src/main/java/br/com/gabriel/contact_list/controllers/ContactController.java
+++ src/main/java/br/com/gabriel/contact_list/controllers/ContactController.java
@@ -5,13 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gabriel.contact_list.dtos.CreateContactDto;
+import br.com.gabriel.contact_list.dtos.UpdateContactDto;
 import br.com.gabriel.contact_list.entitites.Contact;
 import br.com.gabriel.contact_list.services.ContactService;
 
@@ -29,8 +33,49 @@ public class ContactController {
 	}
 	
 	@GetMapping
-	public List<Contact> getAll() {
-		List<Contact> contacts = contactService.getAllProducts();
-		return contacts;
+	public ResponseEntity<List<Contact>> getAll() {
+		List<Contact> contacts = contactService.getAllContacts();
+		if(contacts.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}else {
+			return ResponseEntity.status(HttpStatus.OK).body(contacts);
+		}
+	}
+	
+	@GetMapping("/{id_contact}")
+	public ResponseEntity<Contact> getById(@PathVariable long id_contact) {
+		Contact contact = contactService.getContactById(id_contact);
+		return ResponseEntity.status(HttpStatus.OK).body(contact);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<Void> deleteAll() {
+		 List<Contact> contacts = contactService.deleteAllContacts();
+		 if (contacts == null || contacts.isEmpty()) {
+			 return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); 
+	     }else {
+	    	 return ResponseEntity.status(HttpStatus.OK).build();  
+	     }
+	}
+	
+	@DeleteMapping("/{id_contact}")
+	public ResponseEntity<Contact> deleteById(@PathVariable long id_contact) {
+		Contact contact = contactService.deleteContactId(id_contact);
+		if(contact == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}else{
+			return ResponseEntity.status(HttpStatus.OK).body(contact);
+		}	
+	}
+	
+	@PutMapping("/{id_contact}") 
+	public ResponseEntity<Contact> updateById(@RequestBody UpdateContactDto updateContactDto, @PathVariable long id_contact) {
+		Contact updatedContact = contactService.updateContactById(id_contact, updateContactDto);
+		
+		if (updatedContact == null) {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	    } else {
+	        return ResponseEntity.status(HttpStatus.OK).body(updatedContact); 
+	    }
 	}
 }
--- src/main/java/br/com/gabriel/contact_list/services/ContactService.java
+++ src/main/java/br/com/gabriel/contact_list/services/ContactService.java
@@ -1,11 +1,11 @@
 package br.com.gabriel.contact_list.services;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.gabriel.contact_list.dtos.CreateContactDto;
+import br.com.gabriel.contact_list.dtos.UpdateContactDto;
 import br.com.gabriel.contact_list.entitites.Contact;
 import br.com.gabriel.contact_list.repositories.ContactRepository;
 
@@ -26,13 +26,65 @@ public class ContactService {
 		return contactSaved;
 	}
 	
-	public List<Contact> getAllProducts() {
+	public List<Contact> getAllContacts() {
 		var contacts = contactRepository.findAll();
 		if(contacts.isEmpty()) {
-			System.out.println("Users not found");
-			return contacts;
+			return null;
 		}else{
 			return contacts;
+		}
+	}
+	
+	public Contact getContactById(long id_contact) {
+		var contact = contactRepository.findById(id_contact);
+		if(contact.isPresent()) {
+			return contact.get();
+		}else{
+			return null;
+		}
+	}
+	
+	public List<Contact> deleteAllContacts() {
+		var contacts = contactRepository.findAll();
+		if(contacts.isEmpty()) {
+			return null;
+		}else{
+			contactRepository.deleteAll();
+			return contactRepository.findAll();
+		}
+	}
+	
+	public Contact deleteContactId(long id_contact) {
+	    var contact = contactRepository.findById(id_contact);
+	    if (contact.isPresent()) {  
+	        contactRepository.deleteById(id_contact); 
+	        return contact.get();  
+	    } else {
+	        return null; 
+	    }
+	}
+	
+	public Contact updateContactById(long id_contact, UpdateContactDto updateContactDto) {
+		var contactExists = contactRepository.findById(id_contact);
+		if(contactExists.isPresent()) {
+			var contact = contactExists.get();
+			
+			if(updateContactDto.name() != null) {
+				contact.setName(updateContactDto.name());
+			}
+			if (updateContactDto.imageUrl() != null) {
+	            contact.setImageUrl(updateContactDto.imageUrl());
+	        }
+	        if (updateContactDto.telephoneNumber() != null) {
+	            contact.setTelephoneNumber(updateContactDto.telephoneNumber());
+	        }
+	        if (updateContactDto.contactDescription() != null) {
+	            contact.setContactDescription(updateContactDto.contactDescription());
+	        }
+	        
+			return contactRepository.save(contact);
+		}else{
+			return null;
 		}
 	}
 }
